a738bab5e50013bb47931a3b5c1c69b597fffb14
Koray Ates <koray_s01@hotmail.com>
2016-01-31 00:59:53 +0200

    Odev tamamlandi

--- odev.php
+++ odev.php
@@ -1,5 +1,28 @@
 <?php
+$notlar = array("koray"=>50,"cagatay"=>30);
 
-//sadsadasdsadadsdsadas
-# Sýralama iþlemlerit
+$miniumDeger= min($notlar);#minimum deðeri bulduk.
+$maximumDeger= max($notlar);#maximum deðeri bulduk.
+
+arsort($notlar);#Büyükten küçüðe sýraladýk.
+$gecici=0;
+$ort=array_sum($notlar)/count($notlar);#dizideki elemanlarýn toplamýný eleman sayýsýna böldük.
+echo "======Sýralanmýs Hali=====<br>";
+
+foreach ($notlar as $isim=>$not) {
+    //echo $isim."=".$not."<br>";
+    echo "$isim =$not<br> ";
+    $gecici+=pow(($not-$ort),2);#standart sapmanýn bir adýmý tamamlandý.
+}
+$standartSapma=sqrt($gecici/(count($notlar)-1));
+echo "====Ortalama====<br>";
+echo $ort."<br>";
+echo "====Maximum Deger====<br>";
+echo "$maximumDeger<br>";
+
+echo "=====Minimum Deger=====<br> $miniumDeger<br>";
+echo "======Standart Sapma=======<br>";
+
+
+echo $standartSapma."<br>";
 ?>
